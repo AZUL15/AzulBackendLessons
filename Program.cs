69139b0a4fd32380ce07635160589dbe51cf568d
LIBERO\AZUL <lic.cesarortiz@hotmail.com>
2024-10-28 19:22:00 -0600

    +solutions in coments

--- Program.cs
+++ Program.cs
@@ -17,3 +17,38 @@ namespace TAZASTICH
 
 
 
+
+
+// OTRA POSIBLE SOLUCION ,TAKVEZ MAS COMPLEJA PERO CREO QUE SIRVE 
+
+// Console.WriteLine("Write a number");
+//var Number = Convert.ToInt32(Console.ReadLine());
+
+//if ((Number % 2) == 0)
+//{
+//  Console.WriteLine("The number is even.")
+//}
+//else
+//{
+//  Console.WriteLine("The number is odd.");
+//}
+
+//Console.ReadKey();
+//}
+
+// OTRA POSIBLE SOLUCION USANDO SWITCH
+
+//Console.WriteLine("Write a number");
+//var Number = Convert.ToInt32(Console.ReadLine());
+
+//switch (Number % 2)
+//{
+//case 0:
+//Console.WriteLine("The number is even.");
+//break;
+//default:
+//Console.WriteLine("The number is odd.");
+
+//}
+//Console.ReadKey();
+
